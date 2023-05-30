e39a96293d69a663d44107c2c738c7918f5023d0
aymane <77883489+oaymn@users.noreply.github.com>
2023-05-31 00:35:00 +0100

    Add `findAllByOwnerId` method to the `BankAccountRepository`

--- src/main/java/dev/oaymn/digitalbankingbackend/bankaccount/BankAccountRepository.java
+++ src/main/java/dev/oaymn/digitalbankingbackend/bankaccount/BankAccountRepository.java
@@ -3,7 +3,11 @@ package dev.oaymn.digitalbankingbackend.bankaccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
+
+    Set<BankAccount> findAllByOwnerId(Long ownerId);
 
 }
